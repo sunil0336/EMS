149f39789ec730da5833a24b4ec19ee9844c6ad3
sunil0336 <sunilrathod.0336@gmail.com>
2025-07-05 18:42:15 +0530

    working on admindashboard

--- src/components/others/AllTasks.jsx
+++ src/components/others/AllTasks.jsx
@@ -1,41 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../context/AuthContext';
 
 function AllTasks() {
-  return (
-    <div id='tasklist' className='p-5 mt-5 rounded bg-zinc-800 h-64 overflow-auto'>
-        <div className='bg-red-400 mb-2 py-3 px-5 flex justify-between rounded'>
-            <h2>Sunil</h2>
-            <h3 >Task Title</h3>
-            <h5>Status</h5>
+    const authData = useContext(AuthContext);
+    return (
+        <div id='tasklist' className='py-5 px-10 mt-5 rounded bg-zinc-800 h-64 overflow-auto'>
+            {authData.employees.map((employee) => (
+                    employee.tasks.map((task, index) => (
+                        <div key={index} className='bg-zinc-700 border-2 border-emerald-500 mb-2 py-3 px-5 flex justify-between rounded'>
+                            <h2>{employee.name}</h2>
+                            <h3>{task.title}</h3>
+                            <h5>
+                              {task.newTask
+                                ? 'New Task'
+                                : task.active
+                                ? 'Accepted'
+                                : task.completed
+                                ? 'Completed'
+                                : 'Failed'}
+                            </h5>
+                        </div>
+                    ))
+                ))}
         </div>
-        <div className='bg-green-400 mb-2 py-3 px-5 flex justify-between rounded'>
-            <h2>Sunil</h2>
-            <h3 >Task Title</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-yellow-400 mb-2 py-3 px-5 flex justify-between rounded'>
-            <h2>Sunil</h2>
-            <h3 >Task Title</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-blue-400 mb-2 py-3 px-5 flex justify-between rounded'>
-            <h2>Sunil</h2>
-            <h3 >Task Title</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-red-400 mb-2 py-3 px-5 flex justify-between rounded'>
-            <h2>Sunil</h2>
-            <h3 >Task Title</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-gray-400 mb-2 py-3 px-5 flex justify-between rounded'>
-            <h2>Sunil</h2>
-            <h3 >Task Title</h3>
-            <h5>Status</h5>
-        </div>
-
-    </div>
-  )
+    )
 }
 
 export default AllTasks
+
+{/* <div className='bg-red-400 mb-2 py-3 px-5 flex justify-between rounded'>
+            <h2>Sunil</h2>
+            <h3 >Task Title</h3>
+            <h5>Status</h5>
+        </div> */}
